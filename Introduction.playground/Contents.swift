919c496adb6d296297fb8b578c9cb47798b61685
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-07-04 13:34:35 -0500

    updated introduction playground

--- Introduction.playground/Contents.swift
+++ Introduction.playground/Contents.swift
@@ -7,6 +7,33 @@ import SwiftFoundationAppleBridge
 var str = "Hello, SwiftFoundation"
 
 //: SwiftFoundation is a Cross-Platform, Protocol-Oriented Programming base library to complement the Swift Standard Library.
-//: You can use it in places wher
+//: You can use it in places where Apple's Foundation library is unavailible (e.g. Linux)
+
+let randomUUID = UUID()
+
+//: SwiftFoundation aims to provide the exact same *interface* and *implementation* as Foundation, but upgraded for Swift and Protocol-Oriented Programming where feasible.
+
+let validRawUUID = "7ADBFDE5-0311-441F-AA77-CC7BBECFA949"
+
+let uuid = UUID(rawValue: validRawUUID)
+
+let foundationUUID = NSUUID(stringUUID: validRawUUID)
+
+uuid.rawValue == foundationUUID.UUIDString
+
+//: SwiftFoundation's Protocol-Oriented Programming paradigm permits for toll free bridging between OS-dependent implementations.
+
+func printUUID(uuid: UUIDType) {
+
+    print(uuid.rawValue)
+}
+
+extension NSUUID: UUIDType {}
+
+printUUID(foundationUUID) // Foundation-based
+
+printUUID(uuid) // POSIX-based
+
+uuid.rawValue == foundationUUID.rawValue
 
 
