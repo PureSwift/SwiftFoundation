af8da97d3729f741d9924395b92c112668c9b246
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-07-18 17:39:37 -0500

    updated documentation for Copying protocol

--- Source/Copying.swift
+++ Source/Copying.swift
@@ -7,17 +7,22 @@
 //
 
 /// Protocol for classes that can create equivalent duplicates of themselves.
-/// *Use case:* Classes that incapsuate information that need to be passed by referenced (can't be structs). but need copying for thread safety, immutablility etc.
+///
+/// Duplicate is implied to be of the same mutablility as the reciever.
+/// The copy returned is immutable if the consideration “immutable vs. mutable” applies to the receiving object; otherwise the exact nature of the copy is determined by the class.
+///
+/// **Use Case:** Classes that incapsuate information that need to be passed by reference (can't be structs), but need copying for thread safety, immutablility etc.
 public protocol Copying: class {
     
-    /// Makes a copy of the reciever.
-    /// Duplicate is implied to be of the same mutablility as the reciever.
+    /// Creates a copy of the reciever.
     var copy: Self { get }
 }
 
 /// Protocol for classes that have mutable and immutable types.
-public protocol MutableCopying: Copying {
+///
+/// Only classes that define an “immutable vs. mutable” distinction should adopt this protocol. Classes that don’t define such a distinction should adopt ```Copying``` instead.
+public protocol MutableCopying: class, Copying {
     
-    /// Makes a mutable copy of the reciever.
+    /// Creates a mutable copy of the reciever.
     var mutableCopy: Self { get }
 }
