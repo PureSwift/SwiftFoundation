8dd7ce133b54807484762b686e303658546dae58
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-07-23 17:04:57 -0500

    implemented basic JSON support

--- Source/JSON.swift
+++ Source/JSON.swift
@@ -10,12 +10,12 @@ public typealias JSONArray = [JSONValue]
 
 public typealias JSONObject = [String: JSONValue]
 
-public enum JSONValue {
+public enum JSONValue: RawRepresentable {
     
     case Null
     
     /// JSON value is a String Value
-    case String(JSONString)
+    case String(StringValue)
     
     /// JSON value is a Number Value (specific subtypes)
     case Number(JSONNumber)
@@ -25,23 +25,127 @@ public enum JSONValue {
     
     /// JSON value a JSON object
     case Object(JSONObject)
+    
+    // MARK: RawRepresentable
+    
+    public var rawValue: Any {
+        
+        switch self {
+            
+        case .Null: return Null
+            
+        case .String(let string): return string
+            
+        case .Number(let number): return number.rawValue
+            
+        case .Array(let array): return array.rawValues
+            
+        case .Object(let dictionary):
+            
+            var dictionaryValue = [StringValue: Any](minimumCapacity: dictionary.count)
+            
+            for (key, value) in dictionary {
+                
+                dictionaryValue[key] = value.rawValue
+            }
+            
+            return dictionaryValue
+        }
+    }
+    
+    public init?(rawValue: Any) {
+        
+        guard (rawValue as? NullValue) == nil else {
+            
+            self = .Null
+            return
+        }
+        
+        if let string = rawValue as? StringValue {
+            
+            self = .String(string)
+            return
+        }
+        
+        if let number = JSONNumber(rawValue: rawValue) {
+            
+            self = .Number(number)
+            return
+        }
+        
+        if let rawArray = rawValue as? [Any], let jsonArray: JSONArray = JSONValue.fromRawValues(rawArray) {
+            
+            self = .Array(jsonArray)
+            return
+        }
+        
+        if let rawDictionary = rawValue as? [StringValue: Any] {
+            
+            var jsonObject = JSONObject(minimumCapacity: rawDictionary.count)
+            
+            for (key, rawValue) in rawDictionary {
+                
+                guard let jsonValue = JSONValue(rawValue: rawValue) else { return nil }
+                
+                jsonObject[key] = jsonValue
+            }
+            
+            self = .Object(jsonObject)
+            return
+        }
+        
+        return nil
+    }
 }
 
-public enum JSONNumber {
+public enum JSONNumber: RawRepresentable {
     
     case Boolean(Bool)
     
     case Integer(Int)
     
-    /* Compiler error
-    case Float(Float)
+    case Float(FloatValue)
     
-    case Double(Double)
+    case Double(DoubleValue)
     
-    case Decimal(Decimal)
-    */
+    case Decimal(DecimalValue)
+    
+    // MARK: RawRepresentable
+    
+    public var rawValue: Any {
+        
+        switch self {
+        case .Boolean(let value): return value
+        case .Integer(let value): return value
+        case .Float(let value):   return value
+        case .Double(let value):  return value
+        case .Decimal(let value): return value
+        }
+    }
+    
+    public init?(rawValue: Any) {
+        
+        if let value = rawValue as? Bool            { self = .Boolean(value) }
+        if let value = rawValue as? Int             { self = .Integer(value) }
+        if let value = rawValue as? FloatValue      { self = .Float(value)   }
+        if let value = rawValue as? DoubleValue     { self = .Double(value)  }
+        if let value = rawValue as? DecimalValue    { self = .Decimal(value) }
+        
+        return nil
+    }
 }
 
-// Typealiases only due to compiler error
+// Typealiases due to compiler error
 
-public typealias JSONString = String
+public typealias StringValue = String
+
+public typealias FloatValue = Float
+
+public typealias DoubleValue = Double
+
+public typealias DecimalValue = Decimal
+
+public typealias NullValue = Null
+
+
+
