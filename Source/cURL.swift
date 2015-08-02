bb4ed6d2228e86798ab76651e79c3844a48bd6b4
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-08-02 03:01:11 -0500

    working on cURL

--- Source/cURL.swift
+++ Source/cURL.swift
@@ -11,7 +11,7 @@ import cURL
 /// Class that encapsulates cURL handler.
 public final class cURL {
     
-    private let internalHandler: UnsafeMutablePointer<Void>
+    private let internalHandler: curl_handler
     
     deinit {
         
@@ -27,20 +27,11 @@ public final class cURL {
     
     public func setOption(option: Option) throws {
         
-        switch option {
-            
-        case .URL(let value):
-            
-            let code = curl_easy_setopt(internalHandler, option: CURLOPT_URL, param: value)
-            
-            if let error = Error(code: code) { throw error }
-            
-        case .Port(let value):
-            
-            let code = curl_easy_setopt(internalHandler, option: CURLOPT_PORT, param: value)
-            
-            if let error = Error(code: code) { throw error }
-        }
+        let (optionFlag, value) = option.rawValue
+        
+        let code = curl_easy_setopt(internalHandler, option: optionFlag, param: value as! CVarArgType)
+        
+        if let error = Error(code: code) { throw error }
         
         self.options.append(option)
     }
@@ -50,7 +41,21 @@ public final class cURL {
         let code = curl_easy_perform(internalHandler)
         
         if let error = Error(code: code) { throw error }
+        
+        curl_easy_getinfo(internalHandler, info: CURLINFO, param: UnsafeMutablePointer<>)
     }
+    
+    public var info: [Info] {
+        
+        for rawInfoValue in 1...CURLINFO_LASTONE.rawValue {
+            
+            let info = CURLINFO(rawValue: rawInfoValue)
+            
+            
+        }
+    }
+    
+    // MARK: - Supporting Types
     
     public enum Option {
         
@@ -58,12 +63,28 @@ public final class cURL {
         
         case Port(UInt)
         
+        case Verbose(Bool)
+        
         public var rawValue: (CURLoption, Any) {
             
             switch self {
                 
             case .URL(let value): return (CURLOPT_URL, value)
             case .Port(let value): return (CURLOPT_PORT, Int(value))
+            case .Verbose(let value): return (CURLOPT_VERBOSE, Int(value))
+            }
+        }
+    }
+    
+    public enum Info {
+        
+        case EffectiveURL(String)
+        
+        public var rawValue: (CURLINFO, Any) {
+            
+            switch self {
+                
+            case .EffectiveURL(let value): return (CURLINFO_EFFECTIVE_URL, value)
             }
         }
     }
@@ -71,18 +92,38 @@ public final class cURL {
     public enum Error: ErrorType {
         
         case UnsupportedProtocol
-        
         case FailedInitialization
+        case BadURLFormat
+        case NotBuiltIn
+        case CouldNotResolveProxy
+        case CouldNotResolveHost
+        case CouldNotConnect
+        case FTPBadServerReply
+        case RemoteAccessDenied
         
         public init?(code: CURLcode) {
+                        
+            switch code {
             
-            switch code.rawValue {
+            case CURLE_OK:                     return nil
+            case CURLE_UNSUPPORTED_PROTOCOL:   self = .UnsupportedProtocol
+            case CURLE_FAILED_INIT:            self = .FailedInitialization
+            case CURLE_URL_MALFORMAT:          self = .BadURLFormat
+            case CURLE_NOT_BUILT_IN:           self = .NotBuiltIn
+            case CURLE_COULDNT_RESOLVE_PROXY:  self = .CouldNotResolveProxy
+            case CURLE_COULDNT_RESOLVE_HOST:   self = .CouldNotResolveHost
+            case CURLE_COULDNT_CONNECT:        self = .CouldNotConnect
+            case CURLE_FTP_WEIRD_SERVER_REPLY: self = .FTPBadServerReply
+            case CURLE_REMOTE_ACCESS_DENIED:   self = .RemoteAccessDenied
+            case CURLE_COULDNT_RESOLVE_HOST:   self = .FailedInitialization
+            case CURLE_COULDNT_RESOLVE_HOST:   self = .FailedInitialization
+            case CURLE_COULDNT_RESOLVE_HOST:   self = .FailedInitialization
+            case CURLE_COULDNT_RESOLVE_HOST:   self = .FailedInitialization
+            case CURLE_COULDNT_RESOLVE_HOST:   self = .FailedInitialization
+            case CURLE_COULDNT_RESOLVE_HOST:   self = .FailedInitialization
+            case CURLE_COULDNT_RESOLVE_HOST:   self = .FailedInitialization
                 
-            case CURLE_UNSUPPORTED_PROTOCOL.rawValue: self = .UnsupportedProtocol
-                
-            case CURLE_UNSUPPORTED_PROTOCOL.rawValue: self = .FailedInitialization
-                
-            default: return nil
+            default: fatalError("cURL Error code not handled \(code)")
             }
         }
     }
@@ -91,6 +132,14 @@ public final class cURL {
 
 // MARK: - Function declarations
 
-@asmname("curl_easy_setopt") func curl_easy_setopt(curl: UnsafeMutablePointer<Void>, option: CURLoption, param: CVarArgType...) -> CURLcode
+public typealias curl_handler = UnsafeMutablePointer<Void>
 
+@asmname("curl_easy_setopt") public func curl_easy_setopt(curl: curl_handler, option: CURLoption, param: CVarArgType...) -> CURLcode
+
+@asmname("curl_easy_getinfo") public func curl_easy_getinfo(curl: curl_handler, info: CURLINFO, param: CVarArgType...) -> CURLcode
+
+public func ~= (lhs: CURLcode, rhs: CURLcode) -> Bool {
+    
+    return lhs.rawValue ~= rhs.rawValue
+}
 
---
+++ SwiftFoundation/SwiftFoundationTests/cURLTests.swift
@@ -0,0 +1,40 @@
+//
+//  cURLTests.swift
+//  SwiftFoundation
+//
+//  Created by Alsey Coleman Miller on 8/2/15.
+//  Copyright © 2015 PureSwift. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftFoundation
+
+class cURLTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testSetOption() {
+        
+        let curl = cURL()
+        
+        try! curl.setOption(cURL.Option.URL("http://google.com"))
+        
+        try! curl.setOption(cURL.Option.Port(80))
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
