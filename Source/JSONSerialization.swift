985d92e5d4f02dd1f17938dd5efe6811470e89bf
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-08-22 02:58:27 -0500

    added JSON serialization

---
+++ Source/JSONSerialization.swift
@@ -0,0 +1,19 @@
+//
+//  JSONSerialization.swift
+//  SwiftFoundation
+//
+//  Created by Alsey Coleman Miller on 8/22/15.
+//  Copyright © 2015 PureSwift. All rights reserved.
+//
+
+import JSON
+
+public extension JSON.Value {
+    
+    /// Serializes the JSON to a string.
+    func toString() -> StringValue? {
+        
+        
+    }
+}
+
