a98677a4dc33c2dc42258d4ee7f6ec3909af166a
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-09-22 15:25:57 -0500

    working on FileManager

--- Source/FileManager.swift
+++ Source/FileManager.swift
@@ -16,13 +16,13 @@ public final class FileManager {
     // MARK: - Determining Access to Files
     
     /// Determines whether a file descriptor exists at the specified path. Can be regular file, directory, socket, etc.
-    public static func itemExists(atPath path: String) -> Bool {
+    public static func itemExists(path: String) -> Bool {
         
         return (stat(path, nil) == 0)
     }
     
     /// Determines whether a file exists at the specified path.
-    public static func fileExists(atPath path: String) -> Bool {
+    public static func fileExists(path: String) -> Bool {
         
         var inodeInfo = stat()
         
@@ -36,7 +36,7 @@ public final class FileManager {
     }
     
     /// Determines whether a directory exists at the specified path.
-    public static func directoryExists(atPath path: String) -> Bool {
+    public static func directoryExists(path: String) -> Bool {
         
         var inodeInfo = stat()
         
@@ -54,10 +54,8 @@ public final class FileManager {
     /// Attempts to change the current directory
     public static func changeCurrentDirectory(newCurrentDirectory: String) throws {
         
-        guard chdir(newCurrentDirectory) == 0 else {
-            
-            throw POSIXError.fromErrorNumber!
-        }
+        guard chdir(newCurrentDirectory) == 0
+            else { throw POSIXError.fromErrorNumber! }
     }
     
     /// Gets the current directory
@@ -76,16 +74,16 @@ public final class FileManager {
     
     // MARK: - Creating and Deleting Items
     
-    public static func createFile(atPath path: String, contents: Data? = nil, attributes: FileAttributes = FileAttributes()) throws {
+    public static func createFile(path: String, contents: Data? = nil, attributes: FileAttributes = FileAttributes()) throws {
         
-        
+        fatalError()
     }
     
-    public static func createDirectory(atPath path: String, withIntermediateDirectories createIntermediates: Bool = false, attributes: FileAttributes = FileAttributes()) throws {
+    public static func createDirectory(path: String, withIntermediateDirectories createIntermediates: Bool = false, attributes: FileAttributes = FileAttributes()) throws {
         
         if createIntermediates {
             
-            fatalError("Not Implemented")
+            fatalError("Create Intermediate Directories Not Implemented")
         }
         
         guard mkdir(path, attributes.st_mode) == 0 else {
@@ -94,7 +92,7 @@ public final class FileManager {
         }
     }
     
-    public static func removeItem(atPath path: String) throws {
+    public static func removeItem(path: String) throws {
         
         guard remove(path) == 0 else {
             
@@ -104,36 +102,36 @@ public final class FileManager {
     
     // MARK: - Creating Symbolic and Hard Links
     
-    public static func createSymbolicLink(atPath path: String, withDestinationPath destinationPath: String) throws {
+    public static func createSymbolicLink(path: String, withDestinationPath destinationPath: String) throws {
         
-        
+        fatalError()
     }
     
-    public static func linkItemAtPath(path: String, toPath destinationPath: String) throws {
+    public static func linkItem(path: String, toPath destinationPath: String) throws {
         
-        
+        fatalError()
     }
     
-    public static func destinationOfSymbolicLink(atPath path: String) throws -> String {
+    public static func destinationOfSymbolicLink(path: String) throws -> String {
         
         fatalError()
     }
     
     // MARK: - Moving and Copying Items
     
-    public static func copyItem(atPath sourcePath: String, toPath destinationPath: String) throws {
+    public static func copyItem(sourcePath: String, toPath destinationPath: String) throws {
         
-        
+        fatalError()
     }
     
-    public static func moveItem(atPath sourcePath: String, toPath destinationPath: String) throws {
+    public static func moveItem(sourcePath: String, toPath destinationPath: String) throws {
         
-        
+        fatalError()
     }
     
     // MARK: - Getting and Setting Attributes
     
-    public static func attributesOfItem(atPath path: String) throws -> FileAttributes {
+    public static func attributesOfItem(path: String) throws -> FileAttributes {
         
         return try FileAttributes(path: path)
     }
@@ -141,6 +139,8 @@ public final class FileManager {
     public static func setAttributes(attributes: FileAttributes, ofItemAtPath path: String) throws {
         
         // let originalAttributes = try self.attributesOfItem(atPath: path)
+        
+        fatalError("Not Implemented")
     }
     
     public static func attributesOfFileSystem(forPath path: String) throws -> FileSystemAttributes {
@@ -150,10 +150,23 @@ public final class FileManager {
     
     // MARK: - Getting and Comparing File Contents
     
-    public static func contents(atPath path: String) -> Data? {
+    public static func contents(path: String) -> Data? {
         
-        guard self.fileExists(atPath: path)
-            else { return nil }
+        // get file descriptor for path
+        let file = open(path)
+        
+        defer { fclose(file) } // close file
+        
+        guard fileDescriptor != nil else { return nil }
+        
+        // get file size
+        guard let attributes = try? FileAttributes(path: path) else { return nil }
+        
+        let memoryPointer = UnsafeMutablePointer<Byte>.alloc(attributes)
+        
+        fread()
+        
+        
         
         return []
     }
