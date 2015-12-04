87f9b7b1f9b39738f0f2c704a831fbe3bdf3c54a
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-12-04 01:11:33 -0500

    updated package

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftFoundation",
     dependencies: [
-        .Package(url: "../JSON", majorVersion: 1)
+        .Package(url: "../JSON", majorVersion: 1),
         .Package(url: "../b64", majorVersion: 1)
     ]
 )
