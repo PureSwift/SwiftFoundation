0f4d5562beca3ee5af822ad5046865b1786ef611
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-12-05 18:17:44 -0500

    made CompileError rawValue based on platform

--- Sources/RegularExpressionCompileError.swift
+++ Sources/RegularExpressionCompileError.swift
@@ -15,7 +15,7 @@
 public extension RegularExpression {
     
     // POSIX Regular Expression compilation error
-    public enum CompileError: Int32, ErrorType {
+    public enum CompileError: POSIXRegularExpression.ErrorCode, ErrorType {
         
         /// Invalid use  of repetition operators such as using '*' as the first character.
         ///
