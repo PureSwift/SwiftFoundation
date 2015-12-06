b83eac531b92c8f48439e9dfd7e46b1f93d4989f
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-12-05 18:00:41 -0800

    added example module

---
+++ Sources/Example/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  SwiftFoundationExample
+//
+//  Created by Alsey Coleman Miller on 12/5/15.
+//  Copyright © 2015 PureSwift. All rights reserved.
+//
+
+import SwiftFoundation
+
+print("Hello, World!")
+
+let uuid = UUID()
+
+print("Random UUID: " + uuid.rawValue)
+
