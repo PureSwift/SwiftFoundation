bd6f13f3c14c3f637ce6a0e4113df72e41c3a49d
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-12-16 01:32:51 -0500

    updated documentation script

--- GenerateDocumentation.sh
+++ GenerateDocumentation.sh
@@ -1,9 +1,9 @@
-cd SwiftFoundation
+cd Xcode
 jazzy \
   --clean \
   --author PureSwift \
   --author_url https://github.com/PureSwift \
   --github_url https://github.com/PureSwift/SwiftFoundation \
-  --module-version 1.0.2 \
+  --module-version 1.1.0 \
   --module SwiftFoundation \
   --output ../docs/ \
