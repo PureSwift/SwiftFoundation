954981cca756527daece9545d8cc5597f10fa607
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-01-11 12:56:24 -0500

    deleted documentation
    Jazzy is not working

--- GenerateDocumentation.sh
+++
@@ -1,9 +0,0 @@
-cd Xcode
-jazzy \
-  --clean \
-  --author PureSwift \
-  --author_url https://github.com/PureSwift \
-  --github_url https://github.com/PureSwift/SwiftFoundation \
-  --module-version 1.1.0 \
-  --module SwiftFoundation \
-  --output ../docs/ \
