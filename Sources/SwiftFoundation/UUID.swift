ffd66ab75d816bcf2eb4d3d4fee72a30d6f6ed3b
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-04-01 22:14:03 -0500

    #24 added DataConvertible to UUID

--- Sources/SwiftFoundation/UUID.swift
+++ Sources/SwiftFoundation/UUID.swift
@@ -18,11 +18,13 @@ public struct UUID: ByteValueType, RawRepresentable, CustomStringConvertible {
     
     // MARK: - Initialization
     
+    /// Creates a random UUID.
     public init() {
         
         self.byteValue = POSIXUUIDCreateRandom()
     }
     
+    /// Initializes a UUID with the specified bytes.
     public init(byteValue: ByteValue) {
         
         self.byteValue = byteValue
@@ -44,5 +46,24 @@ public extension UUID {
     var rawValue: String {
         
         return POSIXUUIDConvertToString(byteValue)
+    }
+}
+
+// MARK: - DataConvertible
+
+extension UUID: DataConvertible {
+    
+    public init?(data: Data) {
+        
+        let byteValue = data.byteValue
+        
+        guard byteValue.count == 16 else { return nil }
+        
+        self.init(byteValue: (byteValue[0], byteValue[1], byteValue[2], byteValue[3], byteValue[4], byteValue[5], byteValue[6], byteValue[7], byteValue[8], byteValue[9], byteValue[10], byteValue[11], byteValue[12], byteValue[13], byteValue[14], byteValue[15]))
+    }
+    
+    public func toData() -> Data {
+        
+        return Data(byteValue: [byteValue.0, byteValue.1, byteValue.2, byteValue.3, byteValue.4, byteValue.5, byteValue.6, byteValue.7, byteValue.8, byteValue.9, byteValue.10, byteValue.11, byteValue.12, byteValue.13, byteValue.14, byteValue.15])
     }
 }
