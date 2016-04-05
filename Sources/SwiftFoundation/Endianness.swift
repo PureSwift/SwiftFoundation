a2f55d51c226f64e13299cf1395c65371e2356dc
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-04-05 01:52:43 -0500

    working on endianness and UInt16 extensions

--- Sources/SwiftFoundation/Endianness.swift
+++ Sources/SwiftFoundation/Endianness.swift
@@ -27,34 +27,8 @@ public extension EndianConvertible {
     /// Converts the number to the current endianness.
     var currentEndian: Self {
         
-        if isBigEndian {
-            
-            return bigEndian
-            
-        } else {
-            
-            return littleEndian
-        }
+        return isBigEndian ? bigEndian : littleEndian
     }
 }
 
 public let isBigEndian = 10.bigEndian == 10
-
-public extension UInt16 {
-    
-    /// Initializes value from two little endian ordered bytes. 
-    public init(littleEndian value: (UInt8, UInt8)) {
-        
-        self = (UInt16(value.0).littleEndian + UInt16(value.1).bigEndian).currentEndian
-    }
-    
-    public var littleEndianBytes: (UInt8, UInt8) {
-        
-        let value = self.littleEndian
-        
-        let lowerByte = value & 0xff
-        let higherByte = value >> 8
-        
-        return (UInt8(truncatingBitPattern: lowerByte), UInt8(truncatingBitPattern: higherByte))
-    }
-}
