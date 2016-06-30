253a05519a35417f1cd8cf7560912f4ce02cb4a9
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-06-30 03:04:20 -0500

    Improved UUID: Equatable performance

--- Sources/SwiftFoundation/UUID.swift
+++ Sources/SwiftFoundation/UUID.swift
@@ -102,6 +102,28 @@
             return toData().hashValue
         }
     }
+    
+    // MARK: - Equatable
+    
+    public func == (lhs: UUID, rhs: UUID) -> Bool {
+        
+        return lhs.bytes.0 == rhs.bytes.0 &&
+            lhs.bytes.1 == rhs.bytes.1 &&
+            lhs.bytes.2 == rhs.bytes.2 &&
+            lhs.bytes.3 == rhs.bytes.3 &&
+            lhs.bytes.4 == rhs.bytes.4 &&
+            lhs.bytes.5 == rhs.bytes.5 &&
+            lhs.bytes.6 == rhs.bytes.6 &&
+            lhs.bytes.7 == rhs.bytes.7 &&
+            lhs.bytes.8 == rhs.bytes.8 &&
+            lhs.bytes.9 == rhs.bytes.9 &&
+            lhs.bytes.10 == rhs.bytes.10 &&
+            lhs.bytes.11 == rhs.bytes.11 &&
+            lhs.bytes.12 == rhs.bytes.12 &&
+            lhs.bytes.13 == rhs.bytes.13 &&
+            lhs.bytes.14 == rhs.bytes.14 &&
+            lhs.bytes.15 == rhs.bytes.15
+    }
 
 #endif
 
