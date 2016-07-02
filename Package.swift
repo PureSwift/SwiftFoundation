98e85ce883fb90324369bddad6f29639157169ad
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-07-02 03:39:56 -0500

    Updated SwiftPM package

--- Package.swift
+++ Package.swift
@@ -2,17 +2,17 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftFoundation",
-    dependencies: [
-        .Package(url: "https://github.com/PureSwift/CUUID.git", majorVersion: 1),
-        .Package(url: "https://github.com/PureSwift/CStatfs.git", majorVersion: 1),
-        .Package(url: "https://github.com/PureSwift/CJSONC.git", majorVersion: 1)
-    ],
     targets: [
         Target(
             name: "UnitTests",
             dependencies: [.Target(name: "SwiftFoundation")]),
         Target(
             name: "SwiftFoundation")
+    ],
+    dependencies: [
+        .Package(url: "https://github.com/PureSwift/CUUID.git", majorVersion: 1),
+        .Package(url: "https://github.com/PureSwift/CStatfs.git", majorVersion: 1),
+        .Package(url: "https://github.com/PureSwift/CJSONC.git", majorVersion: 1)
     ],
     exclude: ["Xcode", "Carthage"]
 )
