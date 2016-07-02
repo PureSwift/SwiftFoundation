375a4295e954cdb8f5677fd69eeaea6be8c23f3e
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-07-02 03:41:15 -0500

    Remove PureSwift/CUUID as a dependency

--- Package.swift
+++ Package.swift
@@ -10,7 +10,6 @@ let package = Package(
             name: "SwiftFoundation")
     ],
     dependencies: [
-        .Package(url: "https://github.com/PureSwift/CUUID.git", majorVersion: 1),
         .Package(url: "https://github.com/PureSwift/CStatfs.git", majorVersion: 1),
         .Package(url: "https://github.com/PureSwift/CJSONC.git", majorVersion: 1)
     ],
