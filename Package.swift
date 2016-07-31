3b754756a2d65b42df542977aef2f63e509a924b
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-07-31 17:00:24 -0500

    Moved test source files to "Tests" folder

--- Package.swift
+++ Package.swift
@@ -2,13 +2,6 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftFoundation",
-    targets: [
-        Target(
-            name: "UnitTests",
-            dependencies: [.Target(name: "SwiftFoundation")]),
-        Target(
-            name: "SwiftFoundation")
-    ],
     dependencies: [
         .Package(url: "https://github.com/PureSwift/CStatfs.git", majorVersion: 1),
         .Package(url: "https://github.com/PureSwift/CJSONC.git", majorVersion: 1)
