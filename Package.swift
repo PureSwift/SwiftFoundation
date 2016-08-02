5559f95b55d673fe0f524ef9ca0cf6adcf80caf9
lvs(zikuan.ly) <lvscar@gmail.com>
2016-08-02 16:49:31 +0800

    delete no dependence

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
     name: "SwiftFoundation",
     dependencies: [
-        .Package(url: "https://github.com/PureSwift/CUUID.git", majorVersion: 1),    
         .Package(url: "https://github.com/PureSwift/CStatfs.git", majorVersion: 1),
         .Package(url: "https://github.com/PureSwift/CJSONC.git", majorVersion: 1)
     ],
